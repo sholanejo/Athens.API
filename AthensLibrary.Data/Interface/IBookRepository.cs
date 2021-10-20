ab5658753f12f1e149b675142d7bea353502b764
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-10-20 07:51:14 +0100

    soft delete for books

---
+++ AthensLibrary.Data/Interface/IBookRepository.cs
@@ -0,0 +1,14 @@
+﻿using AthensLibrary.Model.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AthensLibrary.Data.Interface
+{
+    public interface IBookRepository : IRepository<Book>
+    {
+        void Delete(Guid Id);
+    }
+}
