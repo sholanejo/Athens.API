3bc0e9260ee994d16726646ac0ee174a0c77a6da
Ehueriken Samuel obuse <ehuerikenbaba@gmail.com>
2021-10-20 07:10:46 +0100

    implemented soft delete on Author

--- AthensLibrary.Data/Implementations/AuthorRepository.cs
+++ AthensLibrary.Data/Implementations/AuthorRepository.cs
@@ -3,23 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AthensLibrary.Data.Interface;
 using AthensLibrary.Model.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace AthensLibrary.Data.Implementations
 {
-    public class AuthorRepository : Repository<Author>
+    public class AuthorRepository : Repository<Author>,IAuthorRepository
     {
-        private readonly AthensDbContext context;
+        
 
-        public AuthorRepository(DbContext context) : base(context)
+        public AuthorRepository(AthensDbContext context) : base(context)
         {
             //this.context = context;
         }
 
         public void run(Guid AuthorId)
         {
-            context.Authors.Include(c => c.User).ToList();
+            this._dbContext.Authors.Include(c => c.User).ToList();
+        }
+
+        public void Delete(Guid Id)
+        {
+            var author = _dbSet.Find(Id);
+            this._dbContext.Entry(author).State = EntityState.Modified;
+            this._dbContext.SaveChanges();
+
         }
     }
 }
