810564110d9c8eb71091ce9abe545aa4c9db2a20
ogubuike Alex <ogubuikealex@gmail.com>
2021-10-30 08:27:07 +0100

    Changed return model from value tuple to Concrete class

---
+++ AthensLibrary.Model/Helpers/HelperClasses/ReturnModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AthensLibrary.Model.Helpers.HelperClasses
+{
+    public sealed class ReturnModel
+    {
+        public ReturnModel()
+        {
+
+        }
+        public ReturnModel(bool success, string message)
+        {
+            Success = success;
+            Message = message;
+        }
+        public bool Success { get; set; }
+        public string Message { get; set; }       
+    }
+}
